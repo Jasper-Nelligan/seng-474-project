7411cec56e1686707cca6afa7015b9f3d54259c6
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-11-12 16:26:33 -0800

    Add h5_getters.py from MSD repo

--- readMetadata.py
+++ readMetadata.py
@@ -1,15 +1,10 @@
 import tables
+import hdf5_getters
 
 # Open the HDF5 file
-filename = "C:/Users/jaspe/Downloads/millionsongsubset/MillionSongSubset/B/I/D/TRBIDYF128F42680C4.h5"
-file = tables.open_file(filename, mode='r')
+h5 = hdf5_getters.open_h5_file_read("C:/Users/jaspe/Downloads/millionsongsubset/MillionSongSubset/B/I/D/TRBIDYF128F42680C4.h5")
 
-# Access the metadata table
-metadata_table = file.root.metadata.songs
-
-# Iterate over rows in the metadata table and print artist name and album
-for row in metadata_table:
-    print(f"Artist Name: {row['artist_name']}, Album: {row['release']}")
+print(hdf5_getters.get_title(h5))
 
 # Close the file when done
-file.close()
+h5.close()
