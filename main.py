259215e9164323a64529243b7a071d014943cab4
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-11-25 16:36:48 -0800

    Get multiple track features at once

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
-from spotify_api import get_spotify_access_token, get_track_features, get_track_id
+from spotify_api import get_spotify_access_token, get_multiple_track_features, get_track_id
 import os
 from dotenv import load_dotenv
 import sqlite3
+import time
 
 load_dotenv()
 
@@ -10,34 +11,34 @@ client_id = os.getenv('SPOTIFY_CLIENT_ID')
 client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 tags_include = [
-    "rock",
-    "pop",
-    "alternative",
-    "indie",
-    "electronic",
-    "dance",
-    "jazz",
-    "singer-songwriter",
+    # "rock",
+    #"pop",
+    #"alternative",
+    #"indie",
+    #"electronic",
+    #"dance",
+    #"jazz",
+    # "singer-songwriter",
     "metal",
-    "soul",
-    "folk",
-    "instrumental",
-    "punk",
+    #"soul",
+    #"folk",
+    #"instrumental",
+    #"punk",
     "ambient",
-    "hip-hop",
-    "country",
+    #"hip-hop",
+    #"country",
 ]
 
 def get_spotify_features(artist, track, release):
     track_id = get_track_id(artist, track, release)
+
     if track_id:
-        return get_track_features(track_id)
+        return get_multiple_track_features(track_id)
     else:
         print('Track not found in Spotify database:', artist, track, release)
         return None
 
-if __name__ == "__main__":
-    print("Starting script...")
+def main():
     # Initialize the Spotify API
     get_spotify_access_token(client_id, client_secret)
 
@@ -49,6 +50,7 @@ if __name__ == "__main__":
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS combined_data (
         track_id TEXT PRIMARY KEY,
+        spotify_id TEXT,
         artist TEXT,
         title TEXT,
         release TEXT,
@@ -74,14 +76,17 @@ if __name__ == "__main__":
     songs_added_to_db = num_entries
     max_songs = 30000
     spotify_error_occured = False
+    spotify_ids_batch = []
     for root, _, files in os.walk("lastfm_train"):
         if spotify_error_occured:
             print("Spotify error occured, skipping remaining songs")
             break
+
         for file in files:
             if spotify_error_occured:
                 print("Spotify error occured, skipping remaining songs")
                 break
+
             if songs_added_to_db >= max_songs:
                 break
 
@@ -101,58 +106,95 @@ if __name__ == "__main__":
                 if top_tag == "":
                     continue
                 else:
-                    song_id = os.path.splitext(file)[0]
+                    msd_id = os.path.splitext(file)[0]
 
-                    cursor.execute('SELECT * FROM songs WHERE track_id = ?', (song_id,))
+                    # Fetch song data from the songs table
+                    cursor.execute('SELECT * FROM songs WHERE track_id = ?', (msd_id,))
                     track_entry = cursor.fetchone()
 
-                    cursor.execute('SELECT 1 FROM combined_data WHERE track_id = ?', (song_id,))
+                    if not track_entry:
+                        print(f"No track entry found for track ID {msd_id}")
+                        continue
+
+                    cursor.execute('SELECT 1 FROM combined_data WHERE track_id = ?', (msd_id,))
                     entry_exists = cursor.fetchone() is not None
                     
                     # If the song data is already in the combined_data table, skip it
                     if entry_exists:
-                        print("Entry already exists for", song_id)
-                    else:
-                        title = track_entry[1]
-                        release = track_entry[3]
-                        artist = track_entry[6]
+                        print("Entry already exists for", msd_id)
+                        continue
 
-                        try:
-                            # Get Spotify features for the song
-                            spotify_features = get_spotify_features(artist, title, release)
-                            if not spotify_features:
-                                cursor.execute('''
-                                INSERT OR REPLACE INTO combined_data (track_id, artist, title, release, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo)
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                                ''', (song_id, artist, title, release, top_tag, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1))
-                                songs_added_to_db += 1
-                                print(songs_added_to_db, "songs added to the database")
-                                conn.commit()
-                                continue
-                        except Exception as e:
-                            print('Error getting Spotify features: ', e)
-                            spotify_error_occured = True
-                            break
+                    title = track_entry[1]
+                    release = track_entry[3]
+                    artist = track_entry[6]
 
-                        # Insert the new entry into the combined_data table
+                    try:
+                        spotify_id = get_track_id(artist, title, release)
+
                         cursor.execute('''
-                        INSERT OR REPLACE INTO combined_data (track_id, artist, title, release, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                        ''', (song_id, artist, title, release, top_tag,
-                              spotify_features.get('danceability', None),
-                              spotify_features.get('energy', None),
-                              spotify_features.get('key', None),
-                              spotify_features.get('loudness', None),
-                              spotify_features.get('mode', None),
-                              spotify_features.get('speechiness', None),
-                              spotify_features.get('acousticness', None),
-                              spotify_features.get('instrumentalness', None),
-                              spotify_features.get('liveness', None),
-                              spotify_features.get('valence', None),
-                              spotify_features.get('tempo', None)))
+                        INSERT OR REPLACE INTO combined_data (track_id, spotify_id, artist, title, release, genre, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, tempo)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        ''', (msd_id, spotify_id, artist, title, release, top_tag, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1))
                         songs_added_to_db += 1
-                        print(songs_added_to_db, "songs added to the database")
                         conn.commit()
+
+                        if spotify_id:
+                            spotify_ids_batch.append(spotify_id)
+                            print("Track IDs list length:", len(spotify_ids_batch))
+
+                        # Get Spotify features for the current batch of track IDs
+                        if len(spotify_ids_batch) == 99:
+                            print("Getting Spotify features for", len(spotify_ids_batch), "tracks")
+                            spotify_features = get_multiple_track_features(spotify_ids_batch)
+                            time.sleep(10)
+                            if not spotify_features:
+                                print("Error getting Spotify features for batch")
+                                spotify_ids_batch = []
+                                continue
+
+                            
+                            i = 0
+                            for features in spotify_features:
+                                cursor.execute('''
+                                UPDATE combined_data
+                                SET danceability = ?,
+                                    energy = ?,
+                                    key = ?,
+                                    loudness = ?,
+                                    mode = ?,
+                                    speechiness = ?,
+                                    acousticness = ?,
+                                    instrumentalness = ?,
+                                    liveness = ?,
+                                    valence = ?,
+                                    tempo = ?
+                                WHERE spotify_id = ?
+                                ''', (features.get('danceability', None),
+                                    features.get('energy', None),
+                                    features.get('key', None),
+                                    features.get('loudness', None),
+                                    features.get('mode', None),
+                                    features.get('speechiness', None),
+                                    features.get('acousticness', None),
+                                    features.get('instrumentalness', None),
+                                    features.get('liveness', None),
+                                    features.get('valence', None),
+                                    features.get('tempo', None),
+                                    spotify_ids_batch[i]))
+                                i += 1
+                                conn.commit()
+                            songs_added_to_db += len(spotify_features)
+                            print(len(spotify_features), "songs updated in the database")
+                            spotify_ids_batch = []
+                    except Exception as e:
+                        print('Error getting Spotify features: ', e)
+                        spotify_error_occured = True
+                        break
 
     # Close the connection
     conn.close()
+
+if __name__ == "__main__":
+    print("Starting script...")
+    main()
+    
--- spotify_api.py
+++ spotify_api.py
@@ -38,7 +38,6 @@ def get_track_id(artist, track, album):
         raise Exception("Rate limited by Spotify API")
 
     try:
-        print("The response was: ", response)
         response_data = response.json()
         items = response_data['tracks']['items']
         if items:
@@ -47,16 +46,35 @@ def get_track_id(artist, track, album):
             return None
     except ValueError:
         return None
+    except KeyError:
+        return None
 
-def get_track_features(track_id):
-    url = f'https://api.spotify.com/v1/audio-features/{track_id}'
+def get_multiple_track_features(track_ids):
+    url = f'https://api.spotify.com/v1/audio-features/'
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
+    params = {
+        'ids': ','.join(track_ids)
+    }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 429:
+        print("Rate limited by Spotify API")
+        retry_after = response.headers.get('Retry-After')
+        print(f"Rate limited by Spotify API. Retry after {retry_after} seconds.")
+        raise Exception("Rate limited by Spotify API")
+    
     try:
-        return response.json()
+        response_data = response.json()
+        if 'error' in response_data:
+            print(f"Error in response: {response_data['error']}")
+            return None
+        elif not response_data:
+            print("No response data")
+            return None
+        return response_data.get('audio_features', [])
     except ValueError:
         print("Error: Response is not in JSON format")
         return None
