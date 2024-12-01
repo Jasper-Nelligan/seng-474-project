0d729eb71f2d619c8f4ec4ac5ae6dadc7f158041
Chris Stetchman <chrisstetch@gmail.com>
2024-12-01 12:46:49 -0800

    User input function for genre feature averages

--- visualization.py
+++ visualization.py
@@ -17,6 +17,7 @@ def genreDist(songs):
     plt.ylabel('Count')
     plt.xticks(rotation=45)
 
+# Feature Correlation Heatmap
 def featureCorrelation(songs):
     correlation = songs.corr(numeric_only=True)
     plt.figure(figsize=(12, 8))
@@ -24,12 +25,22 @@ def featureCorrelation(songs):
     plt.title('Feature Correlation Heatmap')
     plt.xticks(rotation=45)
 
-def genreFeatureAvgs(songs, feature):
-    genre_averages = songs.groupby('genre')[feature].mean()
+# Feature Averages by Genre
+def genreFeatureAvgs(songs):
+    features = songs.select_dtypes(include=['number']).columns.tolist()
+
+    print("Select a feature to get average by genre:")
+    for i, feature in enumerate(features):
+        print(f"{i + 1}. {feature}")
+
+    num = int(input("Enter the number corresponding to your choice: "))
+    feature = features[num -1]
+    genre_averages = songs.groupby('genre')[feature].mean().sort_values()
+
     genre_averages.plot(kind='bar')
     plt.title(f'Average {feature.capitalize()} by Genre')
     plt.ylabel(f'Average {feature.capitalize()}')
-    plt.xlabel('Genre', fontsize=14)
+    plt.xlabel('Genre')
     plt.xticks(rotation=45)
 
 def main():
@@ -45,7 +56,7 @@ def main():
     #featureDist(songs_preprocessed)
     #genreDist(songs_preprocessed)
     #featureCorrelation(songs_preprocessed)
-    genreFeatureAvgs(songs_preprocessed, 'energy')
+    genreFeatureAvgs(songs_preprocessed)
 
     plt.show()
 
